c3c632631f7ec36f7922f15ebc66f59b391fe9b0
Shradha <shradha.sgl@gmail.com>
2019-06-20 18:20:35 +0530

    Minor changes

--- search.py
+++ search.py
@@ -1,4 +1,11 @@
+dict = {"confidence", "words", "pretty","trees"}
+
 with open("captions.txt") as openfile:
-    for line in openfile:
-            if "confidence" in line:
-                print(line)
+    y = 1
+    for word in dict:
+        if y==0:
+            break
+        for line in openfile:
+                if word in line:
+                    print(line)
+                    y = input()
